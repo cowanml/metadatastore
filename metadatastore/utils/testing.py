8b7c530d9b4c7e40f047f612b0b9d1ca2a780584
Matt Cowan <cowan@bnl.gov>
2015-04-28 10:20:05 -0400

    need to switch branches :(

--- metadatastore/utils/testing.py
+++ metadatastore/utils/testing.py
@@ -1,23 +1,27 @@
 import uuid
 import os
 from metadatastore.api import db_connect, db_disconnect
+import os
 
 conn = None
-db_name = "mds_testing_disposable_{}".format(str(uuid.uuid4()))
+
+test_db_name = "mds_testing_disposable_{}".format(str(uuid.uuid4()))
+test_db_host = 'localhost'
+test_db_port = '27017'
+test_db_alias = 'mds'
 
 
 def db_setup(collections):
     "Create a fresh database with unique (random) name."
     global conn
-#    os.environ['
     db_disconnect(collections)
-    conn = db_connect(db_name, 'localhost', 27017)
+    conn = db_connect(test_db_name, test_db_host,
+                      int(test_db_port), test_db_alias)
 
 
 def db_teardown(collections, drop_db=None):
     "Drop the fresh database and disconnect."
 
     if drop_db == True:
-        conn.drop_database(db_name)
-
+        conn.drop_database(test_db_name)
     db_disconnect(collections)
